6609c7d201e773a78b8881af2ea34aa53771db40
pauloprcm-cmd <paulo.prcm@gmail.com>
2026-05-25 14:25:25 +0000

    alteracao do botao

--- client/src/components/AddTask.jsx
+++ client/src/components/AddTask.jsx
@@ -59,7 +59,7 @@ const AddTask = ({ onAdd }) => {
       </div>
       
       <button type="submit" className="btn btn-block success">
-        Adicionar Tarefa v1
+        Adicionar Tarefa 
       </button>
       
       <Modal
